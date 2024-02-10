cbd5a54b71dafe37cd62945383d777fa263527dd
davidmoshal <dmoshal@gmail.com>
2024-02-10 15:43:48 -0800

    wip

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+import {query} from "./api/api"
+console.log({query})
+
 </script>
 
 <template>
