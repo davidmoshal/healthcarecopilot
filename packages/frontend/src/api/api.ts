75e03d51d14d2231a7eeb0e70e2b353fec309e02
Jordan Checkoff <jordan.checkoff@gmail.com>
2024-02-10 15:57:34 -0800

    merge

--- packages/frontend/src/api/api.ts
+++ packages/frontend/src/api/api.ts
@@ -3,14 +3,17 @@ import OpenAI from "openai";
 const apiKey = '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';
 const openai = new OpenAI({apiKey});
 
-export async function query(prompt:string) {
+export async function query(prompt: string) {
+    let output = ""
     const stream = await openai.chat.completions.create({
         model: "gpt-4",
         messages: [{ role: "user", content: prompt}],
         stream: true,
     });
     for await (const chunk of stream) {
-        console.log(chunk.choices[0]?.delta?.content || "");
+        output += chunk.choices[0]?.delta?.content || "";
     }
+
+    return output
 }
 
